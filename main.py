21f64c808353e04eef49ae273c015621f80ef094
Andrej Byrtus <a.byrtus99@gmail.com>
2020-10-21 18:34:25 +0200

    final

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from kivy.app import App
 from scapy.all import *
 from packet import *
-from mainPage import MainPage
 import json
-
-main_page = MainPage()
 
 
 def netAnalyzer(file_name):
-    print(f'Opening: {file_name}')
+    print(f'Opening file: {file_name}')
+    print(f'...')
 
     # scapy
     packets = rdpcap(file_name)
@@ -18,28 +15,268 @@ def netAnalyzer(file_name):
 
     i = 0
     packet_list = []
-
+    print("analyzing file...")
     for pkt in packets:
         i += 1
-        print(f'{i}. packet: \n {pkt}')
-        packet_list.append(Packet(bytes(pkt), i, packet_headers[i-1]))
+        packet_list.append(Packet(bytes(pkt), i, packet_headers[i - 1]))
+
+    # Print all packets
+    # for pkt in packet_list:
+    #     pkt.pretty_print()
+    print("Done")
+    return packet_list
+
+
+def printMostIPs():
+    res = Packet.GetMostDestination()
+    print(f'Most used destination ip:{res[0]} ({res[1]})')
+
+
+def printDestinationIPs():
+    i = 0
+    print("--Destination IPs--")
+    for ip in Packet.DestinationIPs:
+        i += 1
+        print(f'{i} -- {ip} ({Packet.DestinationIPs[ip]})')
+    print("--------END--------\n")
+
+
+def printAll(packet_list):
     for pkt in packet_list:
-        print(f'frame {pkt.index}')
-        print(f'frame length from pcap API {pkt.frame_len}')
-        print(f'frame length in source {pkt.data_len}')
-        print(f'frame type: {pkt.type[0]} \nlength/ethertype: {pkt.type[1]}')
-        print(f'Source MAC address: {pkt.source_mac}')
-        print(f'Destination MAC address: {pkt.destination_mac}')
-        pkt.print()
-    main_page.populate_list(packet_list)
+        pkt.pretty_print()
 
 
-class MainApp(App):
-    def build(self):
-        return main_page
+def printApplications(packet_list, port):
+    application_list = []
+    application_filtered_list = []
+    for pkt in packet_list:
+        if pkt.source_port == port or pkt.destination_port == port:
+            application_list.append(pkt)
+            application_filtered_list.append(pkt)
+    index = 0
+    all_comms = []
+    for pkt in application_list:
+        src_port = pkt.source_port
+        dest_port = pkt.destination_port
+        index += 1
+        comm = []
+        if pkt.destination_port == port and pkt.syn:
+            comm.append(pkt)
+            for n in application_list[index:]:
+                if n.finres and n.source_port == port:
+                    comm.append(n)
+                    if n in application_filtered_list:
+                        application_filtered_list.remove(n)
+                    break
+                elif (n.source_port == src_port and n.destination_port == dest_port) or (n.source_port == dest_port and n.destination_port == src_port):
+                    comm.append(n)
+                    if n in application_filtered_list:
+                        application_filtered_list.remove(n)
+        if len(comm) > 0:
+            all_comms.append(comm)
+    index = 0
+    for comm in all_comms:
+        index += 1
+        print(f"---Communicaiton {index}---")
+        for pkt in comm:
+            pkt.pretty_print()
+        print(f"---End Communicaiton {index}---")
+
+    print(f'---Unfinished comms---')
+    for pkt in application_filtered_list:
+        pkt.pretty_print()
+    print(f'---END Unfinished comms---')
+
+
+def printARPs(packet_list):
+    arp_packets = []
+    arp_packets_other = []
+    for pkt in packet_list:
+        if pkt.type[1] == "ARP":
+            arp_packets.append(pkt)
+            arp_packets_other.append(pkt)
+    requests = []
+    replies = []
+    for res in arp_packets:
+        if res.arpcode == 2 and res.source_ip != res.destination_ip:
+            replies.append(res)
+            arp_packets_other.remove(res)
+
+    for req in arp_packets:
+        if req.arpcode == 1:
+            index = 0
+            for rep in replies:
+                if rep.source_ip == req.destination_ip and rep.destination_ip == req.source_ip:
+                    requests.append([index, req])
+                    if req in arp_packets_other:
+                        arp_packets_other.remove(req)
+                index += 1
+
+    index = 0
+    for rep in replies:
+        print(f"---ARP communication {index+1}---")
+        for req in requests:
+            if req[0] == index:
+                req[1].pretty_print()
+        rep.pretty_print()
+        print(f"---END ARP {index+1}---\n")
+        index += 1
+    if len(replies) == 0:
+        print("none")
+
+    print("---OTHER ARPs---")
+    for arp in arp_packets_other:
+        arp.pretty_print()
+    if len(arp_packets_other) == 0:
+        print("none")
+    print("")
+
+
+def printTFTPs(packet_list):
+    tftp_packets = []
+    for pkt in packet_list:
+        if pkt.transport_protocol == "UDP":
+            tftp_packets.append(pkt)
+    index = 0
+    all_comms = []
+    for pkt in tftp_packets:
+        comm = []
+        if pkt.destination_port == "tftp (Port 69)":
+            comm.append(pkt)
+            for tftp in tftp_packets[index+1:]:
+                if tftp.destination_port == "tftp (Port 69)":
+                    break
+                elif tftp.source_port == pkt.source_port or tftp.destination_port == pkt.source_port:
+                    comm.append(tftp)
+            index += 1 + len(comm)
+        else:
+            index += 1
+        if len(comm) > 0:
+            all_comms.append(comm)
+    index = 1
+    print("---TFTP Communications---")
+    for comm in all_comms:
+        print(f"---Communication {index}---")
+        if len(comm) > 20:
+            for pkt in comm[:10]:
+                pkt.pretty_print()
+            for pkt in comm[len(comm) - 11:]:
+                pkt.pretty_print()
+        else:
+            for pkt in comm:
+                pkt.pretty_print()
+        print(f"---END Communication {index}---")
+        index += 1
+
+    print("--------END TFTP--------")
+
+
+def printICMPs(packet_list):
+    print("-----ICMP Frames---")
+    unpaired = []
+    requests = []
+    replies = []
+    index = 0
+    icmp_packets = []
+    icmp_requestables = []
+    for requestable in Packet.Headers["icmp_requestable"]:
+        icmp_requestables.append(Packet.Headers["icmp_requestable"][requestable])
+    for pkt in packet_list:
+        if pkt.transport_protocol == "ICMP":
+            icmp_packets.append(pkt)
+    for request in icmp_packets:
+        if request.transport_protocol == "ICMP" and request not in replies and request.icmp_type in icmp_requestables:
+            found = False
+            for reply in icmp_packets[index:]:
+                if reply.transport_protocol == "ICMP" and request.source_mac == reply.destination_mac and request.destination_mac == reply.source_mac:
+                    requests.append(request)
+                    replies.append(reply)
+                    found = True
+                    break
+            if not found:
+                unpaired.append(request)
+        index += 1
+
+    for x in icmp_packets:
+        if x not in requests and x not in replies:
+            unpaired.append(x)
+
+    for x in range(len(requests)):
+        print(f'---Pair {x+1}---')
+        requests[x].pretty_print()
+        replies[x].pretty_print()
+        print(f'---ENDPair {x+1}---\n')
+    print('---other ICMPs---')
+    for x in unpaired:
+        x.pretty_print()
+    print("------ENDICMPs------\n")
+
+
+def loadFile():
+    print("Relative file location:")
+    return netAnalyzer(input())
+
+
+def printHelp():
+    print("load - waits for a relative file path")
+    print("1 - Prints all frames with 3a and 3b")
+    print("3a - Print all destination IPs")
+    print("3b - Prints most frequent destination IP")
+    print("4a - Prints HTTP communications")
+    print("4b - Prints HTTPS communications")
+    print("4c - Prints TELNET communications")
+    print("4d - Prints SSH communications")
+    print("4e - Prints FTP-Control communications")
+    print("4f - Prints FTP-Data communications")
+    print("4g - Prints TFTP communications")
+    print("4h - Prints ICMP pairs and unpaired frames")
+    print("4i - Prints ARP pairs and unpaired frames")
+    print("q - exits")
+    print("help - prints this")
+
 
 if __name__ == '__main__':
-    # netAnalyzer('inputs/vzorky_pcap_na_analyzu/trace-4.pcap')
     load_headers("headers.json")
-    netAnalyzer('inputs/vzorky_pcap_na_analyzu/trace-20.pcap')
-    # MainApp().run()
+    # netAnalyzer('inputs/vzorky_pcap_na_analyzu/trace-4.pcap')
+    # default = 'inputs/vzorky_pcap_na_analyzu/trace-10.pcap'  # One ARP
+    # default = 'inputs/vzorky_pcap_na_analyzu/trace-15.pcap'  # ICMP
+    default = 'inputs/vzorky_pcap_na_analyzu/trace-18.pcap'  # ICMP
+    # default = 'inputs/vzorky_pcap_na_analyzu/unpaired_reply.pcap'
+    print(f'Loading default file: {default}')
+    packet_list = netAnalyzer(default)
+    # printICMPs(packet_list)
+    # printMostIPs()
+    print("Help menu:")
+    print("")
+    printHelp()
+    inp = input()
+    while inp != "q":
+        if inp == "1":
+            printAll(packet_list)
+        elif inp == "4a":
+            printApplications(packet_list, "http (Port 80)")
+        elif inp == "4b":
+            printApplications(packet_list, "https (Port 443)")
+        elif inp == "4c":
+            printApplications(packet_list, "telnet (Port 23)")
+        elif inp == "4d":
+            printApplications(packet_list, "ssh (Port 22)")
+        elif inp == "4e":
+            printApplications(packet_list, "ftp-control (Port 21)")
+        elif inp == "4f":
+            printApplications(packet_list, "ftp-data (Port 20)")
+        elif inp == "4g":
+            printTFTPs(packet_list)
+        elif inp == "4h":
+            printICMPs(packet_list)
+        elif inp == "4i":
+            printARPs(packet_list)
+        elif inp == "3a":
+            printDestinationIPs()
+        elif inp == "3b":
+            printMostIPs()
+        elif inp == "load":
+            packet_list = loadFile()
+        elif inp == "help":
+            printHelp()
+        inp = input()
--- packet.py
+++ packet.py
@@ -9,6 +9,17 @@ SNAP = [0xaa, 0xaa]
 
 class Packet:
     Headers = ""
+    DestinationIPs = {}
+
+    @staticmethod
+    def GetMostDestination():
+        most_ips = -1
+        ip = ""
+        for x in Packet.DestinationIPs.keys():
+            if Packet.DestinationIPs[x] > most_ips:
+                most_ips = Packet.DestinationIPs[x]
+                ip = x
+        return ip, most_ips
 
     def __init__(self, hex_packet: bytes, index: int, packet_header):
         self.index = index
@@ -16,13 +27,95 @@ class Packet:
         self.destination_mac = self.get_destination_MAC(hex_packet)
         self.source_mac = self.get_source_MAC(hex_packet)
         self.type = self.get_type(hex_packet)
-        self.frame_len = packet_header.wirelen
-        self.data_len = packet_header.caplen
-        self.total_size = sys.getsizeof(self.data_len) + sys.getsizeof(self.frame_len) + sys.getsizeof(self.type) + sys.getsizeof(self.source_mac) + sys.getsizeof(self.destination_mac) + sys.getsizeof(self.hex_raw) + sys.getsizeof(self.index)
-        # self.data_len = len(hex_packet) + (22 if self.type[1] == "ARP" else 4)
+        self.transport_protocol = "none"
+        self.syn = False
+        self.finres = False
 
-    def get_size(self):
-        return sys.getsizeof(self)
+        self.ipv4_length = -1
+        self.source_port = -1
+        self.destination_port = -1
+
+        self.icmp_type = -1
+        self.icmp_id = -1
+        self.icmp_seq = -1
+
+        self.destination_mac_arp = -1
+        self.source_mac_arp = -1
+        self.arpcode = -1
+
+        self.source_ip = -1
+        self.destination_ip = -1
+        self.frame_len = packet_header.wirelen
+        self.data_len = 64 if packet_header.caplen < 60 else packet_header.caplen + 4
+        # self.total_size = sys.getsizeof(self.data_len) + sys.getsizeof(self.frame_len) + sys.getsizeof(self.type) +
+        # sys.getsizeof(self.source_mac) + sys.getsizeof(self.destination_mac) + sys.getsizeof(self.hex_raw) +
+        # sys.getsizeof(self.index) self.data_len = len(hex_packet) + (22 if self.type[1] == "ARP" else 4)
+        if self.type[1] == "IPV4":
+            self.get_ipv4(hex_packet)
+        elif self.type[1] == "ARP":
+            self.get_arp(hex_packet)
+        if self.transport_protocol == "TCP":
+            self.get_tcp(hex_packet)
+        elif self.transport_protocol == "UDP":
+            self.get_udp(hex_packet)
+        elif self.transport_protocol == "ICMP":
+            self.get_icmp(hex_packet)
+
+    def get_ipv4(self, hex_arr):
+        ip_header_length = (hex_arr[14] & 0b00001111) * 4
+        self.ipv4_length = ip_header_length
+        #print(f'LEN: {ip_header_length}') # IP Header length debug
+        transport_bytes = [hex_arr[23]]
+        self.transport_protocol = find_match(transport_bytes, Packet.Headers["ip"]["protocol"])
+        self.source_ip = f'{hex_arr[26]}.{hex_arr[27]}.{hex_arr[28]}.{hex_arr[29]}'
+        self.destination_ip = f'{hex_arr[30]}.{hex_arr[31]}.{hex_arr[32]}.{hex_arr[33]}'
+        if self.destination_ip in Packet.DestinationIPs:
+            Packet.DestinationIPs[self.destination_ip] += 1
+        else:
+            Packet.DestinationIPs[self.destination_ip] = 1
+
+    def get_icmp(self, hex_arr):
+        type_bytes = [hex_arr[34 + self.ipv4_length - 20]]
+        self.icmp_type = find_match(type_bytes, Packet.Headers["icmp_type"])
+        self.icmp_id = hex_arr[38] * 256 + hex_arr[39]
+        self.icmp_seq = hex_arr[40] * 256 + hex_arr[41]
+
+    def get_tcp(self, hex_arr):
+        source_port_bytes = [hex_arr[34 + self.ipv4_length - 20], hex_arr[35 + self.ipv4_length - 20]]
+        source_port_number = source_port_bytes[0] * 256 + source_port_bytes[1]
+        source_port = find_match(source_port_bytes, Packet.Headers["known_ports"])
+        if not source_port:
+            self.source_port = f'Unknown port ({source_port_bytes[0] * 256 + source_port_bytes[1]})'
+        else:
+            self.source_port = f'{source_port} (Port {source_port_number})'
+        flag_bytes = hex_arr[47 + self.ipv4_length - 20]
+        self.syn = flag_bytes & 2
+        self.finres = flag_bytes & 1 or flag_bytes & 4
+
+        destination_port_bytes = [hex_arr[36 + self.ipv4_length - 20], hex_arr[37 + self.ipv4_length - 20]]
+        destination_port_number = destination_port_bytes[0] * 256 + destination_port_bytes[1]
+        destination_port = find_match(destination_port_bytes, Packet.Headers["known_ports"])
+        if not destination_port:
+            self.destination_port = f'Unknown port ({destination_port_number})'
+        else:
+            self.destination_port = f'{destination_port} (Port {destination_port_number})'
+
+    def get_udp(self, hex_arr):
+        source_port_bytes = [hex_arr[34 + self.ipv4_length - 20], hex_arr[35 + self.ipv4_length - 20]]
+        source_port_number = source_port_bytes[0] * 256 + source_port_bytes[1]
+        source_port = find_match(source_port_bytes, Packet.Headers["known_ports"])
+        if not source_port:
+            self.source_port = f'Unknown port ({source_port_bytes[0] * 256 + source_port_bytes[1]})'
+        else:
+            self.source_port = f'{source_port} (Port {source_port_number})'
+
+        destination_port_bytes = [hex_arr[36 + self.ipv4_length - 20], hex_arr[37 + self.ipv4_length - 20]]
+        destination_port_number = destination_port_bytes[0] * 256 + destination_port_bytes[1]
+        destination_port = find_match(destination_port_bytes, Packet.Headers["known_ports"])
+        if not destination_port:
+            self.destination_port = f'Unknown port ({destination_port_number})'
+        else:
+            self.destination_port = f'{destination_port} (Port {destination_port_number})'
 
     def print(self):
         byte_index = 1
@@ -58,10 +151,32 @@ class Packet:
         res += str(self.frame_len) + "\n"
         res += str(self.data_len) + "\n"
         res += str(self.type[0]) + "\n" + str(self.type[1]) + "\n"
+        res += str(self.transport_protocol) + "\n"
         res += str(self.source_mac) + "\n"
         res += str(self.destination_mac) + "\n"
         res += str(self.format_hex())
         return res
+
+    def pretty_print(self):
+        print(f'frame {self.index}')
+        print(f'frame length from pcap API {self.frame_len}')
+        print(f'frame length in source {self.data_len}')
+        print(f'frame type: {self.type[0]} \nlength/ethertype: {self.type[1]}')
+        print(f'transport protocol: {self.transport_protocol}')
+        if self.type[1] == "IPV4" or self.type[1] == "ARP":
+            print(f'source ip: {self.source_ip}')
+            print(f'destination ip: {self.destination_ip}')
+        if self.type[1] == "ARP":
+            print(f'source arp mac: {self.source_mac_arp}')
+            print(f'destination arp mac: {self.destination_mac_arp}')
+        if self.transport_protocol == "UDP" or self.transport_protocol == "TCP":
+            print(f'Source port: {self.source_port}')
+            print(f'Destination port: {self.destination_port}')
+        if self.icmp_type != -1:
+            print(f'ICMP Type: {self.icmp_type}')
+        print(f'Source MAC address: {self.source_mac}')
+        print(f'Destination MAC address: {self.destination_mac}')
+        self.print()
 
     @staticmethod
     def get_destination_MAC(hex_arr):
@@ -82,29 +197,38 @@ class Packet:
         len_type_bytes = [hex_arr[12], hex_arr[13]]
         ieee_bytes = [hex_arr[14], hex_arr[15]]
         length = len_type_bytes[0] * 256 + len_type_bytes[1]
-
-        if 46 <= length <= 1500:
-            return "IEEE 802.3" + Packet.get_8023_type(ieee_bytes), "length: " + str(length)
-        elif match_bytes(len_type_bytes, IPX):
-            return "Novell RAW", "length: " + str(length)
-        elif match_bytes(len_type_bytes, SNAP):
-            return "IEEE 802.3 LLC + SNAP" + str(length)
+        if length <= 1500:
+            return Packet.get_8023_type(ieee_bytes), "length: " + str(length)
         else:
             return "Ethernet II", Packet.get_ethertype(hex_arr)
 
     @staticmethod
     def get_ethertype(hex_arr):
         t = [hex_arr[12], hex_arr[13]]
-        return find_match(t, Packet.Headers["ethertype"])
+        return find_match(t, Packet.Headers["ethertype"]) or "unknown"
+
 
     @staticmethod
     def get_8023_type(t):
         if match_bytes(t, IPX):
-            return "Novell RAW"
+            return "IEEE 802.3 IPX RAW"
         elif match_bytes(t, SNAP):
             return "IEEE 802.3 LLC + SNAP"
         else:
             return "IEEE 802.3 LLC"
+
+    def get_arp(self, hex_arr):
+        src_mac = ""
+        for hex_byte in hex_arr[22:28]:
+            src_mac += hex(hex_byte)[2:].zfill(2) + " "
+        self.source_mac_arp = src_mac
+        dest_mac = ""
+        for hex_byte in hex_arr[32:38]:
+            dest_mac += hex(hex_byte)[2:].zfill(2) + " "
+        self.destination_mac_arp = dest_mac
+        self.source_ip = f'{hex_arr[28]}.{hex_arr[29]}.{hex_arr[30]}.{hex_arr[31]}'
+        self.destination_ip = f'{hex_arr[38]}.{hex_arr[39]}.{hex_arr[40]}.{hex_arr[41]}'
+        self.arpcode = int(hex_arr[21])
 
 
 def find_match(t, match_dict):
@@ -127,7 +251,6 @@ def match_bytes(x, y):
 
 def load_headers(filename):
     print("---Loading headers: ")
-    print(Packet.Headers)
     Packet.Headers = json.load(open(filename))
 
     print("---Loaded: ")
